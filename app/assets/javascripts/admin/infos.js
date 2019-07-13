4578e2ba9afd377b71852733a2d77a299a63dc7b
Eric-Guo <guochunzhong@thape.com.cn>
2019-07-13 10:03:59 +0800

    Float the toolbar for news editor.

--- app/assets/javascripts/admin/infos.js
+++ app/assets/javascripts/admin/infos.js
@@ -1,6 +1,8 @@
 document.addEventListener("turbolinks:load", function() {
   if ($('#editor').length > 0) {
     return new Simditor({
+      toolbarFloat: true,
+      toolbarFloatOffset: 50,
       textarea: $('#editor'),
       toolbar: ['bold', 'italic', 'fontScale', 'color', '|', 'ol', 'ul', '|', 'image', 'link', '|', 'indent', 'outdent', 'alignment', '|', 'hr', 'table'],
       // fileKey: 'picture'  #指定服务器获取上传文件数据的参数params["upload_file"]
