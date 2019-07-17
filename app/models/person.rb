358868da47683fc567691fabf447eda11883682e
Eric-Guo <guochunzhong@thape.com.cn>
2019-07-17 09:37:10 +0800

    Using url_name to link to the people

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ApplicationRecord
   translates :name, :title, :introduce
+  validates_uniqueness_of :url_name
 
   mount_uploader :avatar, AvatarUploader
   mount_uploader :photo, PhotoUploader
