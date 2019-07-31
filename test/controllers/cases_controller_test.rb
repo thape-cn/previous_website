a6dd49af47508b6ec63c6f45e38027e100e110f2
Eric-Guo <guochunzhong@thape.com.cn>
2019-07-31 11:16:18 +0800

    Case and management controller test.

--- test/controllers/cases_controller_test.rb
+++ test/controllers/cases_controller_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class CasesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get cases_index_url
+    get cases_path
     assert_response :success
   end
 
   test "should get show" do
-    get cases_show_url
+    get case_path(cases(:one))
     assert_response :success
   end
-
 end
