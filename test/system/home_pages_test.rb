f483ca8599fefb70cbbd9977a353d7014307ac1d
Eric-Guo <guochunzhong@thape.com.cn>
2019-07-31 13:44:50 +0800

    Rename home page.

--- test/system/home_pages_test.rb
+++ test/system/home_pages_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class HomeTest < ApplicationSystemTestCase
+class HomePagesTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
     assert_text "武汉福星惠誉福莱中心"
