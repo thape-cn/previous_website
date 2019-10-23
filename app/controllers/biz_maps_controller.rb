f5f48dfe8a0dac9271e006785f5e5e656127ed6d
Eric-Guo <guochunzhong@thape.com.cn>
2019-10-23 12:57:29 +0800

    Now switch to using @contact

--- app/controllers/biz_maps_controller.rb
+++ app/controllers/biz_maps_controller.rb
@@ -1,57 +1,74 @@
 class BizMapsController < ApplicationController
   def index
+    @contact2 = MapContact.find 2
+    @contact3 = MapContact.find 3
+    @contact4 = MapContact.find 4
+    @contact5 = MapContact.find 5
+    @contact6 = MapContact.find 6
+    @contact8 = MapContact.find 8
+    @contact9 = MapContact.find 9
+    @contact10 = MapContact.find 10
+    @contact11 = MapContact.find 11
+    @contact12 = MapContact.find 12
+    @contact13 = MapContact.find 13
+    @contact14 = MapContact.find 14
+    @contact15 = MapContact.find 15
+    @contact16 = MapContact.find 16
+    @contact17 = MapContact.find 17
+    @contact18 = MapContact.find 18
   end
 
   def modal
     @contact_id = params[:id].to_i
+    @contact = MapContact.find params[:id] unless @contact_id.in?([1,7])
     case @contact_id
     when 1
       @contact_title = '上海'
       @contact_subtitle = '建筑、规划、室内、景观、资产管理、能源、审图、VR'
     when 2
-      @contact_title = '沈阳'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 3
-      @contact_title = '北京'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑、规划'
     when 4
-      @contact_title = '西安'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 5
-      @contact_title = '成都'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 6
-      @contact_title = '重庆'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 7
       @contact_title = '武汉'
       @contact_subtitle = '建筑'
     when 8
-      @contact_title = '厦门'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 9
-      @contact_title = '深圳'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 11
-      @contact_title = '天津'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 12
-      @contact_title = '青岛'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 13
-      @contact_title = '南京'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 15
-      @contact_title = '郑州'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 16
-      @contact_title = '杭州'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 17
-      @contact_title = '广州'
+      @contact_title = @contact.name
       @contact_subtitle = '建筑'
     when 18
-      @contact_title = '合肥'
+      @contact_title = @contact.name
       @contact_subtitle = ''
     end
   end
