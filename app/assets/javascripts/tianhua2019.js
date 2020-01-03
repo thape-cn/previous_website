20e99c03908755f06e3a7af16d9cb8288bf7a3d4
Eric-Guo <guochunzhong@thape.com.cn>
2020-01-03 16:05:24 +0800

    Filling data from DB.

--- app/assets/javascripts/tianhua2019.js
+++ app/assets/javascripts/tianhua2019.js
@@ -21,36 +21,36 @@ function setElementGlobal(hypeDocument) {
   setElement('page4-project-name', tianhua2019.page4_project_name, hypeDocument);
   setElement('page4-work-day', tianhua2019.page4_work_day, hypeDocument);
 
-  setElement('page5-complete-area', '6666平方米', hypeDocument);
-  setElement('page5-football-num', 500, hypeDocument);
-  setElement('page5-working-city', '北京、上海、深圳、广州', hypeDocument);
-  setElement('page5-hometown', '温州', hypeDocument);
+  setElement('page5-complete-area', tianhua2019.page5_complete_area, hypeDocument);
+  setElement('page5-football-num', tianhua2019.page5_football_num, hypeDocument);
+  setElement('page5-working-city', tianhua2019.page5_working_city, hypeDocument);
+  setElement('page5-hometown', tianhua2019.page5_hometown, hypeDocument);
 
-  setElement('page6-course-num', 12, hypeDocument);
-  setElement('page6-course-hour', 88, hypeDocument);
-  setElement('page6-course-percent', '96%', hypeDocument);
+  setElement('page6-course-num', tianhua2019.page6_course_num, hypeDocument);
+  setElement('page6-course-hour', tianhua2019.page6_course_hour, hypeDocument);
+  setElement('page6-course-percent', tianhua2019.page6_course_percent, hypeDocument);
 
-  setElement('page7-award', '创新设计奖', hypeDocument);
+  setElement('page7-award', tianhua2019.page7_award, hypeDocument);
 
-  setElement('page8-my-students', 6432, hypeDocument);
-  setElement('page8-my-course', '现代建筑与近代建筑', hypeDocument);
-  setElement('page8-my-attendance', 7654, hypeDocument);
+  setElement('page8-my-students', tianhua2019.page8_my_students, hypeDocument);
+  setElement('page8-my-course', tianhua2019.page8_my_course, hypeDocument);
+  setElement('page8-my-attendance', tianhua2019.page8_my_attendance, hypeDocument);
 
-  setElement('page9-nickname', 'Jedi Knight', hypeDocument);
-  setElement('page9-plugin-name', '插件2<br />插件名字3', hypeDocument);
+  setElement('page9-nickname', tianhua2019.page9_nickname, hypeDocument);
+  setElement('page9-plugin-name', tianhua2019.page9_plugin_name, hypeDocument);
 
-  setElement('page10-name', '过纯中', hypeDocument);
-  setElement('page10-project-name', '商务智能报表项目', hypeDocument);
-  setElement('page10-hours', '4000小时', hypeDocument);
+  setElement('page10-name', tianhua2019.page10_name, hypeDocument);
+  setElement('page10-project-name', tianhua2019.page10_project_name, hypeDocument);
+  setElement('page10-hours', tianhua2019.page10_hours, hypeDocument);
 
-  setElement('page11-name', '王玥', hypeDocument);
+  setElement('page11-name', tianhua2019.page11_name, hypeDocument);
 
-  setElement('page12-name', '冯巧容', hypeDocument);
+  setElement('page12-name', tianhua2019.page12_name, hypeDocument);
 
-  setElement('page13-call-count', 234, hypeDocument);
+  setElement('page13-call-count', tianhua2019.page13_call_count, hypeDocument);
 
-  setElement('page14-mobile-rate', '89%', hypeDocument);
-  setElement('page14-ai-tools-count', 28, hypeDocument);
-  setElement('page14-ai-tools-major', '信息科技专业', hypeDocument);
-  setElement('page14-ai-tools-name', '自动对坐标出图工具', hypeDocument);
+  setElement('page14-mobile-rate', tianhua2019.page14_mobile_rate, hypeDocument);
+  setElement('page14-ai-tools-count', tianhua2019.page14_ai_tools_count, hypeDocument);
+  setElement('page14-ai-tools-major', tianhua2019.page14_ai_tools_major, hypeDocument);
+  setElement('page14-ai-tools-name', tianhua2019.page14_ai_tools_name, hypeDocument);
 }
--- app/controllers/tianhua2019s_controller.rb
+++ app/controllers/tianhua2019s_controller.rb
@@ -10,20 +10,62 @@ class Tianhua2019sController < ApplicationController
     page3_filling_rate = if r.fill_rate.present?
       "#{(r.fill_rate*100).to_i}%"
     end
+    page3_busy_month = max_month(r)
+    page3_busy_workday = max_workday(r)
+
+    page5_complete_area = if r.prj_area.present?
+      "#{r.prj_area.to_i}平方米"
+    end
+    page6_course_percent = if r.study_rank.present?
+      "#{(r.study_rank*100).to_i}%"
+    end
+
+    page10_project_name = if r.max_parter_project3.present?
+      "#{r.max_parter_project1}<br />#{r.max_parter_project2}<br />#{r.max_parter_project3}".html_safe
+    elsif r.max_parter_project2.present?
+      "#{r.max_parter_project1}<br />#{r.max_parter_project2}".html_safe
+    else
+      r.max_parter_project1.presence
+    end
+
     @name = r.name
     @clerkcode = r.clerkcode
+
     @tianhua2019 = {
       page2_year: 2020,
       page2_age: years_between_dates(r.firstday),
       page2_day: (Date.today - r.firstday).floor,
       page3_busy_percent: page3_busy_percent,
-      page3_busy_month: r.max_month&.month,
-      page3_busy_workday: '周四',
+      page3_busy_month: page3_busy_month,
+      page3_busy_workday: page3_busy_workday,
       page3_filling_rate: page3_filling_rate,
       page4_project_num: r.prjno,
       page4_project_owner: r.max_serve_client,
       page4_project_name: r.max_projectname,
-      page4_work_day: r.max_workdays
+      page4_work_day: r.max_workdays,
+      page5_complete_area: page5_complete_area,
+      page5_football_num: ((r.prj_area||0)/137.5).to_i,
+      page5_working_city: r.work_place,
+      page5_hometown: r.home_town,
+      page6_course_num: r.learn_course,
+      page6_course_hour: r.study_hours,
+      page6_course_percent: page6_course_percent,
+      page7_award: r.micro_course,
+      page8_my_students: 6000,
+      page8_my_course: '如何建构一个和谐的大家庭',
+      page8_my_attendance: 7888,
+      page9_nickname: 'Sky Walker',
+      page9_plugin_name: 'X战机<br >蓝色小光剑'.html_safe,
+      page10_name: r.max_parter_name,
+      page10_project_name: page10_project_name,
+      page10_hours: r.max_parter_hours,
+      page11_name: nil,
+      page12_name: r.teacher,
+      page13_call_count: nil,
+      page14_mobile_rate: "90%",
+      page14_ai_tools_count: 25,
+      page14_ai_tools_major: '划水专业',
+      page14_ai_tools_name: '人工智能画图'
     }
   end
 
@@ -44,4 +86,53 @@ class Tianhua2019sController < ApplicationController
     ((date_to - date_from) / 365).ceil
   end
 
+  def max_month(r)
+    max_key = {
+      jan: r.jan,
+      feb: r.feb,
+      mar: r.mar,
+      apr: r.apr,
+      may: r.may,
+      jun: r.jun,
+      jul: r.jul,
+      aug: r.aug,
+      sep: r.sep,
+      oct: r.oct,
+      nov: r.nov,
+      dec: r.dec
+    }.max_by{ |k,v| v||0 }[0]
+
+    {
+      jan: '1',
+      feb: '2',
+      mar: '3',
+      apr: '4',
+      may: '5',
+      jun: '6',
+      jul: '7',
+      aug: '8',
+      sep: '9',
+      oct: '10',
+      nov: '11',
+      dec: '12'
+    }[max_key]
+  end
+
+  def max_workday(r)
+    max_key = {
+      monday: r.monday,
+      tuesday: r.tuesday,
+      wednesday: r.wednesday,
+      thursday: r.thursday,
+      friday: r.friday,
+    }.max_by{ |k,v| v||0 }[0]
+
+    {
+      monday: '周一',
+      tuesday: '周二',
+      wednesday: '周三',
+      thursday: '周四',
+      friday: '周五',
+    }[max_key]
+  end
 end
