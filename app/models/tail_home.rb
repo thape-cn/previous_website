ebc2863983f0b28eebdd808629475b4502d1e145
Eric-Guo <guochunzhong@thape.com.cn>
2020-07-24 15:01:01 +0800

    Typo.

--- app/models/tail_home.rb
+++ app/models/tail_home.rb
@@ -1,7 +1,7 @@
 class TailHome < ApplicationRecord
   mount_uploader :new_project_photo_1, PictureUploader
-  mount_uploader :new_project_photo_2w, PictureUploader
-  mount_uploader :new_project_photo_3l, PictureUploader
+  mount_uploader :new_project_photo_2l, PictureUploader
+  mount_uploader :new_project_photo_3w, PictureUploader
   mount_uploader :new_project_photo_4, PictureUploader
   mount_uploader :new_project_photo_5, PictureUploader
   mount_uploader :new_project_photo_6, PictureUploader
