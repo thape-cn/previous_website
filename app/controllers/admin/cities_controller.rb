eab3a8c78642b0808a90ddbd7e3cfd68a848b8f8
Eric-Guo <guochunzhong@thape.com.cn>
2020-07-27 11:38:08 +0800

    Allow modify the cities.

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class CitiesController < Admin::ApplicationController
     before_action :logged_in_user
 
-    before_action :set_admin_user, only: :destroy
+    before_action :set_city, only: [:edit, :update]
 
     def index
       @cities = City.all
@@ -12,18 +12,34 @@ module Admin
       @city = City.new
     end
 
+    def edit
+    end
+
+    def update
+      if @city.update(params[:city].permit!)
+        redirect_to admin_cities_path
+      else
+        render :edit
+      end
+    end
+
     def create
       @city = City.new(params[:city].permit!)
 
       respond_to do |format|
         if @city.save
           flash[:success] = "创建成功!"
-
           format.html { redirect_to admin_cities_path }
         else
           format.html { render :new }
         end
       end
     end
+
+    private
+
+      def set_city
+        @city = City.find(params[:id])
+      end
   end
 end
