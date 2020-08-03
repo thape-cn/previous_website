50c984ea7c5dea545108e1f4755006d98e67ce43
Eric-Guo <guochunzhong@thape.com.cn>
2020-08-03 16:44:26 +0800

    More clear syntax.

--- app/models/service_file.rb
+++ app/models/service_file.rb
@@ -1,11 +1,5 @@
 class ServiceFile < ApplicationRecord
-  translates :building_intro
-  translates :interior_intro
-  translates :planning_intro
-  translates :landscape_intro
-  translates :vetting_intro
-  translates :consluting_intro
-  translates :vrtech_intro
+  translates :building_intro, :interior_intro, :planning_intro, :landscape_intro, :vetting_intro, :consluting_intro, :vrtech_intro
 
   mount_uploader :interior_intro_file, ServiceFileUploader
 end
