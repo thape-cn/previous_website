cdf8fcb1e3f9044d1e12a391894406b59288ae82
Eric-Guo <guochunzhong@thape.com.cn>
2020-08-24 09:52:06 +0800

    Allow export the csv files.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,11 +2,27 @@ module Admin
   class ApplicationController < ::ApplicationController
     layout 'admin'
     before_action :set_active_menu
-    # http_basic_authenticate_with name: "dhh", password: "shin"
 
     def set_active_menu
       @current = ['/' + ['admin', controller_name].join('/')]
     end
 
+    protected
+
+      def render_csv_header(filename = nil)
+        filename ||= params[:action]
+        filename += '.csv'
+
+        if request.env['HTTP_USER_AGENT'] =~ /msie/i
+          headers['Pragma'] = 'public'
+          headers['Content-type'] = 'text/plain'
+          headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+          headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
+          headers['Expires'] = '0'
+        else
+          headers['Content-Type'] ||= 'text/csv'
+          headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
+        end
+      end
   end
 end
--- app/controllers/admin/message_controller.rb
+++ app/controllers/admin/message_controller.rb
@@ -1,9 +1,34 @@
+require 'csv'
+
 module Admin
   class MessageController < Admin::ApplicationController
     before_action :logged_in_user
 
     def index
-      @messages = GuestMessage.all.order(id: :desc).page(params[:page]).per(params[:per_page])
+      messages = GuestMessage.all.order(id: :desc)
+      respond_to do |format|
+        format.html do
+          @messages = messages.page(params[:page]).per(params[:per_page])
+        end
+        format.csv do
+          render_csv_header "thape web messages"
+          csv_res = CSV.generate do |csv|
+            csv << %w[序号 公司 姓名 联系方式 留言 留言时间]
+            messages.each do |s|
+              values = []
+              values << s.id
+              values << s.company
+              values << s.name
+              values << s.contact_details
+              values << s.message
+              values << s.created_at
+              csv << values
+            end
+          end
+          send_data "\xEF\xBB\xBF#{csv_res}"
+        end
+      end
+
     end
 
     def show
