d0fa8c9b814d5414a57a0b967977655fe1c2b7da
Eric-Guo <guochunzhong@thape.com.cn>
2021-03-18 11:16:21 +0800

    Extract the admin.rb

--- config/routes.rb
+++ config/routes.rb
@@ -55,68 +55,8 @@ Rails.application.routes.draw do
   end
   get '/service/:name', to: redirect { |params, req| "/cn/#{params[:name]}" }
 
-  namespace :admin do
-    root 'home#show', as: 'root'
-    resources :home, only: [:show, :edit, :update]
-    resource :tail_home, only: [:edit, :update]
-    resource :about_page, only: [:edit, :update]
-    resource :work_type_page, only: [:edit, :update]
-    resources :about, only: [:show, :edit, :update]
-    resources :people do
-      collection do
-        post :update_positions
-      end
-      member do
-        patch :top
-        patch :bottom
-        patch :up
-        patch :down
-        delete :destory_city_people
-      end
-    end
-    resources :seos, only: [:index, :update]
-    resources :cities, except: [:destroy, :show]
-    resources :map_contacts, only: [:index, :edit, :update]
-    resources :users, except: [:show, :edit, :update]
-    resources :service_files, only: [:show, :edit, :update]
-    resources :cases do
-      member do
-        patch :top
-        patch :bottom
-        patch :up
-        patch :down
-        delete :destory_picture
-      end
-    end
-    resources :works do
-      collection do
-        post :update_positions
-      end
-      member do
-        patch :top
-        patch :bottom
-        patch :up
-        patch :down
-        delete :destory_picture
-      end
-    end
-    resources :infos do
-      member do
-        patch :top
-        patch :bottom
-        patch :up
-        patch :down
-      end
-      collection do
-        post :upload
-      end
-    end
-    resources :pictures, except: [:show]
-    resources :message, only: [:index, :show, :destroy]
-    get '/login', to: 'sessions#new'
-    post '/login', to:'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-  end
+  draw :admin
+
   resources :tianhua2019s, only: [:show, :index]
 
   resource :wechat, only: [:show, :create]
