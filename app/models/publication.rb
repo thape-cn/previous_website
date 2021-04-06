7fd94455cf5c8e76f4e4475fac7d9764393d518b
Eric-Guo <guochunzhong@thape.com.cn>
2021-04-06 17:56:26 +0800

    JpgPngUploader allow PNG.

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,7 +1,7 @@
 class Publication < ApplicationRecord
   translates :title
   mount_uploader :pdf_file, PdfFileUploader
-  mount_uploader :cover_jpg, JpgUploader
+  mount_uploader :cover_jpg, JpgPngUploader
   mount_uploader :cover_webp, WebpUploader
   enum category_status: %i[monographs standard_specification paper_patent]
 end
