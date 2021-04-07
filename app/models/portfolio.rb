735729e4c5a9697be4a40e5a800cd8aef1e48e10
Eric-Guo <guochunzhong@thape.com.cn>
2021-04-07 14:30:33 +0800

    Allow upload the PNG files.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   translates :title, :sub_title
   mount_uploader :pdf_file, PdfFileUploader
-  mount_uploader :cover_jpg, JpgUploader
+  mount_uploader :cover_jpg, JpgPngUploader
   mount_uploader :cover_webp, WebpUploader
 end
