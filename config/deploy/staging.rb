2b43648c35eda611ec4ed042add9ec1d2c50d8da
Eric-Guo <guochunzhong@thape.com.cn>
2021-04-08 13:50:27 +0800

    wechat config fix.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@
 set :nginx_use_ssl, true
 set :deploy_to, "/var/www/thape_old_web"
 set :rails_env, 'staging'
-set :puma_service_unit_name, :thape_old_web
+set :puma_service_unit_name, :puma_thape_old_web
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
