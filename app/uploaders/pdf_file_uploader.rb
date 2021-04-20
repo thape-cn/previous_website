f6ae5d1697c116997da4c07231a3eb898bdf803a
qinjiapeng <759848795@qq.com>
2021-04-20 11:14:33 +0800

    pdf上传新增attachment头标识

--- app/uploaders/pdf_file_uploader.rb
+++ app/uploaders/pdf_file_uploader.rb
@@ -6,4 +6,11 @@ class PdfFileUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(pdf)
   end
+
+  def content_disposition
+    # Only for non-image files
+    if file.extension.downcase.in?(%w(pdf))
+      "attachment;filename=#{file.original_filename}"
+    end
+  end
 end
