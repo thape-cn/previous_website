ed39cc336d20a51f570d0e87d2d50fa69504453f
Eric-Guo <guochunzhong@thape.com.cn>
2021-05-11 17:46:03 +0800

    Original thape_web take down, so thape_web2 rename to thape_web

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :nginx_use_ssl, true
 set :puma_service_unit_name, :puma_old_web
 set :puma_systemctl_user, :system
 
-server 'thape_web2', user: 'thape_web', roles: %w{app db web}
+server 'thape_web', user: 'thape_web', roles: %w{app db web}
