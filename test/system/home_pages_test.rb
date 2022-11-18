9295573ebf00ed4fa8c7dc7824f9ad7b81319845
Eric Guo <eric.guocz@gmail.com>
2022-11-18 11:41:44 +0800

    No system test.

--- test/system/home_pages_test.rb
+++
@@ -1,8 +0,0 @@
-require "application_system_test_case"
-
-class HomePagesTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit root_url
-    assert_text "武汉福星惠誉福莱中心"
-  end
-end
