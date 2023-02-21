8780f2b0ec4da26070d00f50304770d9ffd812de
Eric Guo <eric.guocz@gmail.com>
2023-02-21 10:41:53 +0800

    gem and yarn upgrade

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.17.0'
+lock '~> 3.17.2'
 
 set :application, 'thape_web'
 set :repo_url, 'https://git.thape.com.cn/rails/old-web.git'
