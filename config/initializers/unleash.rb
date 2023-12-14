00d55584539308572e4c79023982cf30919e0da7
Eric Guo <eric.guocz@gmail.com>
2023-12-14 16:29:17 +0800

    No need those files.

--- config/initializers/unleash.rb
+++ config/initializers/unleash.rb
@@ -1,6 +1,9 @@
 Unleash.configure do |config|
   config.instance_id         = 'thape_web_backend'
-  config.app_name            = 'default'
+  config.app_name            = 'thape_old_web'
   config.url                 = 'https://unleash.thape.com.cn/api'
   config.custom_http_headers = {'Authorization': Rails.application.credentials.unleash_token}
+  config.logger   = Rails.logger
 end
+
+UNLEASH = Unleash::Client.new
