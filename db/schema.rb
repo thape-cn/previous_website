bc63ffdb89f811353cc0b8408f0667c501f17eb6
Eric Guo <eric.guocz@gmail.com>
2023-12-26 15:25:59 +0800

    Do DB migrate to Rails 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_023228) do
+ActiveRecord::Schema[7.1].define(version: 2023_06_16_023228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -633,6 +633,26 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_023228) do
     t.string "coordinate_lat"
     t.string "coordinate_lng"
     t.index ["city_id"], name: "index_works_on_city_id"
+  end
+
+  create_table "znzmos", force: :cascade do |t|
+    t.string "url"
+    t.string "keywords"
+    t.string "user_id"
+    t.string "view_count"
+    t.string "download_count"
+    t.string "update_time"
+    t.string "design_date"
+    t.string "design_style"
+    t.string "file_type"
+    t.string "file_page_num"
+    t.string "designer"
+    t.string "developer"
+    t.string "project_area"
+    t.string "official_subsidy_price"
+    t.string "vip_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id", name: "active_storage_attachments_blob_id_fkey"
