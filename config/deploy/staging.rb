3c59ac68a4fb1c8accec3a74242c941bc6cd3c19
Eric Guo <eric.guocz@gmail.com>
2023-12-26 16:45:55 +0800

    Missing puma_systemctl_user in staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,6 @@ set :nginx_use_ssl, true
 set :deploy_to, "/var/www/thape_old_web"
 set :rails_env, 'staging'
 set :puma_service_unit_name, :puma_thape_old_web
+set :puma_systemctl_user, :system
 
 server 'bandwagon', user: 'thape_web', roles: %w{app db web}
